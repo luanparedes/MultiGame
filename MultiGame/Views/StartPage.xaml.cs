e9c8a91c21b89be2c798c95907dc2b6abcc93cba
Luan Paredes <luan.developer.ti@gmail.com>
2022-11-09 22:06:38 -0300

    MG-001: Initial commit

---
+++ MultiGame/Views/StartPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MultiGame.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml.Controls;
+
+namespace MultiGame
+{
+    public partial class StartPage : Page
+    {
+        private StartPageViewModel ViewModel => new StartPageViewModel();
+
+        public StartPage()
+        {
+            this.InitializeComponent();
+            DataContext = ViewModel;
+        }
+    }
+}
